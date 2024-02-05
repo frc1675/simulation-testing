c996e7781442fa7f996e468890c1854d706fe41b
Jake Herrmann <herrmann.jake@protonmail.com>
2024-02-04 19:52:27 -0600

    Created unit test for arm

--- src/main/java/frc/robot/subsystems/TestArmIO.java
+++ src/main/java/frc/robot/subsystems/TestArmIO.java
@@ -1,29 +1,31 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-/** Add your docs here. */
 public class TestArmIO implements IArmIO {
 
     public boolean periodicCalled = false;
     public double currentMotorOutput = 0.0;
     public double currentMeasurement = 0.0;
+
     @Override
     public void setMotorOutput(double power) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setMotorOutput'");
+        currentMotorOutput = power;
     }
 
     @Override
     public double getMeasurement() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMeasurement'");
+        return currentMeasurement;
     }
 
     @Override
     public void periodic() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'periodic'");
-    }}
+        periodicCalled = true;
+    }
+
+    public double getCurrentMotorOutput() {
+        return currentMotorOutput;
+    }
+
+    public void setCurrentMeasurement(double currentMeasurement) {
+        this.currentMeasurement = currentMeasurement;
+    }
+}
---
+++ src/test/java/ArmTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.TestArmIO;
+
+public class ArmTest {
+    
+    private static Arm arm;
+    private static TestArmIO io;
+
+    @BeforeAll
+    public static void setup() {
+        io = new TestArmIO();
+        arm = new Arm(io, null);
+
+        io.setCurrentMeasurement(0);
+    }
+
+    @Test
+    public void testOnTarget() {
+        arm.setTarget(180);        
+
+        assertFalse(arm.onTarget());
+
+        io.setCurrentMeasurement(180);
+        assertTrue(arm.onTarget());
+
+        io.setCurrentMeasurement(180.3);
+        assertTrue(arm.onTarget());
+
+        io.setCurrentMeasurement(179.8);
+        assertTrue(arm.onTarget());
+    }
+
+}
